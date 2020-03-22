a5b929bed3c2a6732fffb27d7a93ddcea8ae3ee5
Ivan Semak <ivanvsemak@gmail.com>
2020-03-22 15:37:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
+  let digit = Math.abs(n%10);  
+  let digitFirst = n/10;
+  let a = Math.trunc(digitFirst);
+  let digitSecond = Math.abs(a%10);
+  let digitThird = a/10;
+  let b =Math.abs(Math.trunc(digitThird));
+  let arr = new Array();
+  arr[0]=digit;
+  arr[1]=digitSecond;
+  arr[2]=b;
+  var joinArray = arr.join("");
+  return joinArray;  
   
 }
